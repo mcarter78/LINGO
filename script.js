7f10d4c55d13d9f3c96a1becf0420e98bb82c6e6
thaddak0 <annabellethaddeus@gmail.com>
2016-02-02 16:13:33 -0800

    add jquery funciton to set up board

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-function draw() {
-var canvas = document.getElementById('grid');
-var ctx = canvas.getContext('2d');
-ctx.strokeRect(10, 10, 100, 100);
+$('#startbutton').click(setGame);
+
+function setGame() {
+	console.log('linked');
+	var idnumber = 1;
+	for (var i = 1; i <= 5; i++) {
+		$('.gameboard').append('<div class="row">').attr("id", "idnumber");
+		for (var j = 1; j<=5; j++) {
+			$('#idnumber').append('<div class="cell">');
+		}	
+	} idnumber ++;
 }
