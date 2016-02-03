fd2aae779440867c649c4fcac9233f366740ed4a
thaddak0 <annabellethaddeus@gmail.com>
2016-02-02 16:38:54 -0800

    add a funciton that selects a new answer from a hard coded array

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
-$(document).ready();
-$('#startbutton').on('click', setGame);
+var answers = ["ghost", "ghoul", "great"];
+var randomIndex;
+var answer;
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function selectAnswer() {
+		randomIndex = getRandomInt(0,2);
+		var currentAnswer = answers[randomIndex];
+		return currentAnswer;
+	}
 
 function setGame() {
 	console.log('linked');
 	var idnumber = 1;
+	
 	for (var i = 1; i <= 5; i++) {
 		$('.gameboard').append('<div class="row">').attr("id", "idnumber");
 		for (var j = 1; j<=5; j++) {
 			$('#idnumber').append('<div class="cell">');
 		}	
 	} idnumber ++;
+	
+	answer = selectAnswer();
+	console.log(answer);
 }
+
+
+
 
