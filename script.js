97e1c574542f3e9b871cc7365434fc6073742c24
thaddak0 <annabellethaddeus@gmail.com>
2016-02-02 17:28:34 -0800

    fixed the funciton setGame so it puts the cells in the right row

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var answers = ["ghost", "ghoul", "great"];
+var lengthOfAnswers = answers.length;
 var randomIndex;
 var answer;
 function getRandomInt(min, max) {
@@ -6,25 +7,47 @@ function getRandomInt(min, max) {
 }
 
 function selectAnswer() {
-		randomIndex = getRandomInt(0,2);
+		randomIndex = getRandomInt(0,lengthOfAnswers);
 		var currentAnswer = answers[randomIndex];
 		return currentAnswer;
 	}
 
+function fillFirstLetter(answer) {
+	var firstLetter = answer[0];
+	$();
+}
+
 function setGame() {
 	console.log('linked');
-	var idnumber = 1;
+
+	$('.gameboard').append('<div class="row" id="firsttry">');
+	$('.gameboard').append('<div class="row" id="secondtry">');
+	$('.gameboard').append('<div class="row" id="thirdtry">');
+	$('.gameboard').append('<div class="row" id="fourthtry">');
+	$('.gameboard').append('<div class="row" id="fifthtry">');
+	for (var j = 0; j < 5; j ++){
+	$('#firsttry').append('<div class ="cell">');
+	$('#secondtry').append('<div class ="cell">');
+	$('#thirdtry').append('<div class ="cell">');
+	$('#fourthtry').append('<div class ="cell">');
+	$('#fifthtry').append('<div class ="cell">');
+	}
+
+
+	// for (var k = 1; k < 6; k++) {
+		// idnumber = k;
+		// $('.row').attr('id', idnumber);
+			// for (var j = 1; j < 6; j++) {
+				// idnumber = j;
+				// $('#idnumber').append('<div class="cell">');
+	// }
+	// }
 	
-	for (var i = 1; i <= 5; i++) {
-		$('.gameboard').append('<div class="row">').attr("id", "idnumber");
-		for (var j = 1; j<=5; j++) {
-			$('#idnumber').append('<div class="cell">');
-		}	
-	} idnumber ++;
-	
+		
 	answer = selectAnswer();
 	console.log(answer);
 }
+
 
 
 
